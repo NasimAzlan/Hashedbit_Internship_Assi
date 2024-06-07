50a869f8c10410e528b4ccbb84381f0d4e097e6e
NASIM ANSARI <148028449+NasimAzlan@users.noreply.github.com>
2024-06-07 07:40:27 +0530

    completed

---
+++ React_Ass3/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import SurveyForm from './assets/Components/SurveyForm'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <SurveyForm />
+    </div>
+  )
+}
+
+export default App
